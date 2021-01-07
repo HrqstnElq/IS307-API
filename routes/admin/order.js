713626eb2b1e56a7d25b857d18d0f8da7b90bc9d
shin☘ <huutrimega@gmail.com>
2021-01-07 11:59:23 +0700

    xong

--- routes/admin/order.js
+++ routes/admin/order.js
@@ -1,7 +1,12 @@
 const router = require("express").Router();
+const Order = require("../../models/order");
 
 router.get("/", (req, res) => {
-	res.send("h");
+	Order.find({})
+		.then((orders) => {
+			res.json(orders);
+		})
+		.catch((err) => res.status(500).json(err));
 });
 
 module.exports = router;
